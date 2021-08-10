f8deb803fd7055a44b8a1c8626a00774b59b0986
BwanaQ <thunjawax@gmail.com>
2021-08-10 10:49:35 +0300

    Add whitenoise and djoser urls

--- DjangoAPI/settings.py
+++ DjangoAPI/settings.py
@@ -39,11 +39,18 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # 3rd Party Apps
+    'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
     'corsheaders',
     'cloudinary',
     'cloudinary_storage',
 ]
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:4200",
+    "http://localhost:4200",
 
+]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -55,11 +62,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:4200",
-    "http://localhost:4200",
-
-]
 
 ROOT_URLCONF = 'DjangoAPI.urls'
 
